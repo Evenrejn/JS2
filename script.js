dc0a4deccc25dceafdc201d8f49ea2c4c50873b6
Evenrejn <brendivajn@gmail.com>
2021-04-16 17:23:09 +0300

    first homework

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const goods = [
+  { title: 'Shirt', price: 150 },
+  { title: 'Socks', price: 50 },
+  { title: 'Jacket', price: 350 },
+  { title: 'Shoes', price: 250 },
+];
+
+const renderGoodsItem = (title = "item", price = "0") => {
+  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+
+const renderGoodsList = (list = goods) => {
+    list.forEach(function(item) {
+        document.querySelector(".goods-list").innerHTML =  document.querySelector(".goods-list").innerHTML + renderGoodsItem(item.title, item.price);
+    });
+};
+
+renderGoodsList(goods);
+
