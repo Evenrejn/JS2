6318c3d3f4a56a26a192fe51af466bbd428eaa90
Evenrejn <brendivajn@gmail.com>
2021-04-29 17:17:47 +0300

    5 homework with vue

--- script.js
+++ script.js
@@ -1,115 +1,146 @@
 'use strict';
 
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+ const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-function makeGETRequest(url) {
-  return new Promise((resolve, reject) => {
-    let xhr;
+const app = new Vue({
+  el: '#app',
+  data: {
+    goods: [],
+    filteredGoods: [],
+    searchLine: '',
+    isVisibleCart: false 
+  },
 
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-    } else if (window.ActiveXObject) { 
-      xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+  methods: {
+    makeGETRequest(url, callback) {
+      return new Promise((resolve, reject) => {
+        let xhr;
+    
+        if (window.XMLHttpRequest) {
+          xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { 
+          xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+         //   if (xhr.status === 200) resolve(xhr.responseText);
+         //   else reject('Error');
+          callback(xhr.responseText);
+          }
+        };
+    
+      // xhr.open('GET', `${API_URL}${url}`, true);
+        xhr.open('GET', url, true);
+        xhr.send();
+      });
+    },
 
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) resolve(xhr.responseText);
-        else reject('Error');
-      }
-    };
+    clickHandler() {
+      app.FilterGoods();
+    },
 
-    xhr.open('GET', `${API_URL}${url}`, true);
-    xhr.send();
-  });
-}
+    clickCart() {
+      app.isVisibleCart = !app.isVisibleCart;
+    },
 
-// const renderGoodsItem = (title = "item", price = "0") => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+    FilterGoods() {
+      const regexp = new RegExp(this.searchLine, 'i');
+      this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
+    },
 
-// const renderGoodsList = (list = []) => {
-//     list.forEach(function(item) {
-//         document.querySelector(".goods-list").innerHTML = document.querySelector(".goods-list").innerHTML + renderGoodsItem(item.title, item.price);
-//     });
-// };
-
-class GoodsItem {
-    constructor(title, price, id_product) {
-      this.title = title;
-      this.price = price;
-      this.id_product = id_product;
-    }
-    render() {
-      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><button id="${this.id_product}">Добавить</button></div>`;
-    }
-}
-
-class GoodsList {
-    constructor() {
-      this.goods = [];
-      this.filteredGoods = [];
-    }
-    fetchGoods() {
-    //   this.goods = [
-    //     { title: 'Shirt', price: 150 },
-    //     { title: 'Socks', price: 50 },
-    //     { title: 'Jacket', price: 350 },
-    //     { title: 'Shoes', price: 250 },
-    //   ];
-    // }
-    return makeGETRequest(`/catalogData.json`)
-      .then((goods) => {
+    mounted() {
+      this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
         this.goods = JSON.parse(goods);
         this.filteredGoods = JSON.parse(goods);
       });
     }
-
-    filterGoods(value) {
-      const regexp = new RegExp(value, 'i');
-      this.filteredGoods = this.goods.filter(good => regexp.test(good.title));
-      this.render();
-    }
-
-    render() {
-      let listHtml = '';
-      this.goods.forEach(good => {
-        const goodItem = new GoodsItem(good.title, good.price, good.id_product);
-        listHtml += goodItem.render();
-      });
-      document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-    totalPrice() {
-        let total = 0;
-        this.goods.forEach(item => {
-            total += item.price;
-        });            
-    }
-}
-
-const list = new GoodsList();
-list.fetchGoods().then(() => list.render());
-
-class shopBasket extends GoodsList {
-    constructor() {
-        super();
-    }
-    fetchGoods() {}
-    buy() {}
-    delete() {}
-}
-
-class shopBasketItem extends GoodsList {
-    constructor() {
-        super();
-    }
-    addOne() {}
-    removeItem() {}
-    deleteItem() {}
-}
-
-const searchInput = document.querySelector('.goods-search');
-searchInput.addEventListener('input', ({target}) => {
-  const {value} = target;
-  list.filterGoods(value);
+  }
 });
 
-//renderGoodsList(goods);
+
+
+// class GoodsItem {
+//     constructor(product_name, price, id_product) {
+//       this.product_name = product_name;
+//       this.price = price;
+//       this.id_product = id_product;
+//     }
+//     render() {
+//       return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p><button id="${this.id_product}">Добавить</button></div>`;
+//     }
+// }
+
+// class GoodsList {
+//     constructor() {
+//       this.goods = [];
+//       this.filteredGoods = [];
+//     }
+    // fetchGoods() {
+    // return makeGETRequest(`/catalogData.json`)
+    //   .then((goods) => {
+    //     this.goods = JSON.parse(goods);
+    //     this.filteredGoods = JSON.parse(goods);
+    //   });
+    // }
+
+    // filterGoods(value) {
+    //   const regexp = new RegExp(value, 'i');
+    //   this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
+    //   this.render();
+    // }
+
+//     render() {
+//       let listHtml = '';
+//       this.goods.forEach(good => {
+//         const goodItem = new GoodsItem(good.product_name, good.price, good.id_product);
+//         listHtml += goodItem.render();
+//       });
+//       document.querySelector('.goods-list').innerHTML = listHtml;
+//     }
+//     totalPrice() {
+//         let total = 0;
+//         this.goods.forEach(item => {
+//             total += item.price;
+//         });            
+//     }
+// }
+
+// const list = new GoodsList();
+// list.fetchGoods().then(() => list.render());
+
+// class shopBasket extends GoodsList {
+//     constructor() {
+//         super();
+//     }
+//     fetchGoods() {}
+//     buy() {}
+//     delete() {}
+// }
+
+// class shopBasketItem extends GoodsList {
+//     constructor() {
+//         super();
+//     }
+//     addOne() {}
+//     removeItem() {}
+//     deleteItem() {}
+// }
+
+// const searchInput = document.querySelector('.goods-search');
+// searchInput.addEventListener('input', ({target}) => {
+//   const {value} = target;
+//   list.filterGoods(value);
+// });
+
+
+
+// Добавить методы и обработчики событий для поля поиска. 
+// Создать в объекте данных поле searchLine и привязать к нему содержимое поля ввода. 
+// На кнопку Искать добавить обработчик клика, вызывающий метод FilterGoods.
+// Добавить корзину. В html-шаблон добавить разметку корзины. 
+// Добавить в объект данных поле isVisibleCart, управляющее видимостью корзины.
+// * Добавлять в .goods-list заглушку с текстом «Нет данных» в случае, если список товаров пуст.
+
+
+
