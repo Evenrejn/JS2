86efeefcd5ffe46e8acc50c10d5b861618a0bc47
Evenrejn <brendivajn@gmail.com>
2021-04-30 10:51:37 +0300

    fifth lesson, vue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
- const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const app = new Vue({
   el: '#app',
@@ -9,6 +9,13 @@ const app = new Vue({
     filteredGoods: [],
     searchLine: '',
     isVisibleCart: false 
+  },
+
+  mounted() {
+    this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      this.filteredGoods = JSON.parse(goods);
+    });
   },
 
   methods: {
@@ -24,13 +31,10 @@ const app = new Vue({
     
         xhr.onreadystatechange = function () {
           if (xhr.readyState === 4) {
-         //   if (xhr.status === 200) resolve(xhr.responseText);
-         //   else reject('Error');
           callback(xhr.responseText);
           }
         };
     
-      // xhr.open('GET', `${API_URL}${url}`, true);
         xhr.open('GET', url, true);
         xhr.send();
       });
@@ -38,109 +42,26 @@ const app = new Vue({
 
     clickHandler() {
       app.FilterGoods();
+
+
+      console.log('clickHandler');
     },
 
     clickCart() {
       app.isVisibleCart = !app.isVisibleCart;
+
+
+      console.log('clickCart');
     },
 
     FilterGoods() {
       const regexp = new RegExp(this.searchLine, 'i');
       this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
+
+
+      console.log('FilterGoods');
     },
 
-    mounted() {
-      this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
-        this.goods = JSON.parse(goods);
-        this.filteredGoods = JSON.parse(goods);
-      });
-    }
   }
 });
-
-
-
-// class GoodsItem {
-//     constructor(product_name, price, id_product) {
-//       this.product_name = product_name;
-//       this.price = price;
-//       this.id_product = id_product;
-//     }
-//     render() {
-//       return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p><button id="${this.id_product}">Добавить</button></div>`;
-//     }
-// }
-
-// class GoodsList {
-//     constructor() {
-//       this.goods = [];
-//       this.filteredGoods = [];
-//     }
-    // fetchGoods() {
-    // return makeGETRequest(`/catalogData.json`)
-    //   .then((goods) => {
-    //     this.goods = JSON.parse(goods);
-    //     this.filteredGoods = JSON.parse(goods);
-    //   });
-    // }
-
-    // filterGoods(value) {
-    //   const regexp = new RegExp(value, 'i');
-    //   this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
-    //   this.render();
-    // }
-
-//     render() {
-//       let listHtml = '';
-//       this.goods.forEach(good => {
-//         const goodItem = new GoodsItem(good.product_name, good.price, good.id_product);
-//         listHtml += goodItem.render();
-//       });
-//       document.querySelector('.goods-list').innerHTML = listHtml;
-//     }
-//     totalPrice() {
-//         let total = 0;
-//         this.goods.forEach(item => {
-//             total += item.price;
-//         });            
-//     }
-// }
-
-// const list = new GoodsList();
-// list.fetchGoods().then(() => list.render());
-
-// class shopBasket extends GoodsList {
-//     constructor() {
-//         super();
-//     }
-//     fetchGoods() {}
-//     buy() {}
-//     delete() {}
-// }
-
-// class shopBasketItem extends GoodsList {
-//     constructor() {
-//         super();
-//     }
-//     addOne() {}
-//     removeItem() {}
-//     deleteItem() {}
-// }
-
-// const searchInput = document.querySelector('.goods-search');
-// searchInput.addEventListener('input', ({target}) => {
-//   const {value} = target;
-//   list.filterGoods(value);
-// });
-
-
-
-// Добавить методы и обработчики событий для поля поиска. 
-// Создать в объекте данных поле searchLine и привязать к нему содержимое поля ввода. 
-// На кнопку Искать добавить обработчик клика, вызывающий метод FilterGoods.
-// Добавить корзину. В html-шаблон добавить разметку корзины. 
-// Добавить в объект данных поле isVisibleCart, управляющее видимостью корзины.
-// * Добавлять в .goods-list заглушку с текстом «Нет данных» в случае, если список товаров пуст.
-
-
 
